c43b4e458b5a8ef733a95ee4f52008ac25c1b756
Mohammed Ihsaan <mohammedihsaan28@gmail.com>
2025-11-08 14:15:14 +0530

    Implements database models

---
+++ database/index.ts
@@ -0,0 +1,7 @@
+//Database models export 
+export {default as Event}  from './event.model';
+export {default as Booking}  from './booking.model';
+
+//Typescript interfaces export
+export type { IEvent } from './event.model';
+export type { IBooking } from './booking.model';
